7402defa25adfdc166b635e83962b56f95d2ca29
ValeraAltf4 <149300269+ValeraAltf4@users.noreply.github.com>
2023-10-29 13:25:29 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//вывод в консоль приветствия миру 
+		print ("Hello, World!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
